780aafc6a7b47c724ff5dd69dc973fe7de89fdfd
Shinshi11a <pro100vanyok2016@gmail.com>
2023-03-20 19:25:09 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿public class Orc
+{
+    public static int TotalGoldCarried { get; private set; } = 0;
+    public int GoldCarried { get; private set; } = 0;
+
+    public Orc()
+    {
+        GoldCarried = 2;
+        TotalGoldCarried += GoldCarried;
+        if (TotalGoldCarried > 10)
+        {
+            GoldCarried -= 2;
+            TotalGoldCarried -= 2;
+        }
+    }
+}
